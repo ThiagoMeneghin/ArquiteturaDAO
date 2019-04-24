d8fd59a55ee971bd5dc46d35b007c8cdf3b9e3ec
unknown <haushu5@gmail.com>
2019-04-24 17:02:29 -0300

    Segundo commit

---
+++ lesson12/MotherDAO.java
@@ -0,0 +1,24 @@
+package br.edu.utfpr.dao;
+
+public class MotherDAO {
+
+    public final void AcessaDAO(int i) {
+        switch (i) {
+            case 1:
+                public abstract void incluir();
+                break;
+            case 2:
+                public abstract List<> listarTodos();
+                break;
+            case 3:
+                public abstract boolean excluir();
+                break;
+            case 4:
+                public abstract boolean alterar();
+                break;
+            case 5:
+                public abstract Generic listarporId();
+                break;
+        }
+    }
+}
